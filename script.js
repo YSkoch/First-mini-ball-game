e7d68767a0ef98dfda4b3ce4b9b87f544d5b8f32
YSkoch <112730436+YSkoch@users.noreply.github.com>
2023-01-14 00:22:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+window.onload = function () {
+    
+    let count = 0;
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var x = 300;
+    var y = 350;
+    var t = Date.now();
+    let speed = 20;
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.fillStyle="red";
+    context.fill();
+    
+   
+       document.onkeydown = function() {
+         count += 1;
+         y -= 25;
+   
+         function draw() {
+         var timePassed = (Date.now()-t)/1000;
+         t = Date.now();
+         context.clearRect(0,0,600,400);
+   
+         context.beginPath();
+         context.arc(x, y, 50, 0, 2 * Math.PI);
+         context.fillStyle="red";
+         context.fill();
+   
+         context.font = '25px Arial';
+         context.fillStyle = 'white';
+         context.fillText("Count:" + count,20,30);
+   
+         if (y <=350) {
+         speed += 25 * timePassed;
+         y += speed*timePassed;
+              }
+         if (y>350) {
+         count = 0;
+              }
+          window.requestAnimationFrame(draw);
+         }
+       
+        draw();
+       }
+    
+   }
